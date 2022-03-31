d0e0f411a7e34a200169005f704e3caac08adf56
Liqin (Lukas) Zhang <lz2809@columbia.edu>
2022-03-31 16:27:46 -0400

    doc: two operators

--- pseudo_software_logic.c
+++ pseudo_software_logic.c
@@ -1,31 +1,27 @@
-// Addition
+// call server side addition operation
 vec cli_addition(vec &c1, vec &c2)
 {
 	return serv_addition(c1, c2);
 }
 
+// server side addition operation, return the vector addition of two ciphtertext
 vec serv_addition(vec &c1, vec &c2)
 {
 	return c1 + c2;
 }
 
-// Linear Transform
+// call server side linear transform operation, send the key switch matrix M from GS -> S to the server
 mat cli_linear_trans(mat &G, mat &S, mat &T)
 {
 	// G is linear transformation matrix
-	// call key_switching_accelerator in the  hardware
-	M = keySwitchMatrix(G * S, T);
+	// call key_switching_accelerator in hardware
+	M = keySwitchMatrix(G * S, T); // in hardware
 	return serv_linear_trans(M, c);
 }
 
+// server side linear transform operation, returns c'==S(Gx) given c=Sx and M
 mat serv_linear_trans(mat &M, vec &c)
 {
-	// c = Sx and M is the key switch matrix from GS to S')
 	c_bit = bit_repr_vector(c); // in hardware
 	return M * c_bit;
-}
-
-mat keySwitchMatrix(mat S, mat T)
-{
-	mat S * = bit_repr_matrix(S); // in hardware
 }
