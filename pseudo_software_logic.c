04d21fa49d10e9241f2a66567228d0f608b68b13
Liqin (Lukas) Zhang <lz2809@columbia.edu>
2022-03-31 17:50:53 -0400

    tempt polynomial

--- pseudo_software_logic.c
+++ pseudo_software_logic.c
@@ -15,13 +15,50 @@ mat cli_linear_trans(mat &G, mat &S, mat &T)
 {
 	// G is linear transformation matrix
 	// call key_switching_accelerator in hardware
-	M = keySwitchMatrix(G * S, T); // in hardware
+	mat M = syscall.keySwitchMatrix(G * S, T); // compute key switch matrix in hardware
 	return serv_linear_trans(M, c);
 }
 
 // server side linear transform operation, returns c'==S(Gx) given c=Sx and M
 mat serv_linear_trans(mat &M, vec &c)
 {
-	c_bit = bit_repr_vector(c); // in hardware
+	c_bit = syscall.bit_repr_vector(c); // compute bit representation in hardware
 	return M * c_bit;
+}
+
+// call server side inner product operation
+// returns M, the key switch matrix from vec(S^t S) to S
+mat cli_inner_product(mat &T, int w, mat &c1, mat &c2)
+{
+	mat S = getSecretKey(T);						// get secret key
+	mat t = transpose(vectorize(transpose(S) * S)); // vec(S^t S) to S,
+	mat M = syscall.keySwitchMatrix(t, T);
+	return serv_inner_product(c1, c2, M, w);
+}
+
+// given two ciphertexts and the keyswitch matrix, computes an inner product
+vec serv_inner_product(vec c1, vec c2, mat M, int w)
+{
+	vec cc = vectorize(c1 * transpose(c2));
+	vec n = syscall.nearestInteger(cc, w);
+	return M * syscall.bit_repr_cector(n);
+}
+
+// call server side polynomial operation
+// returns M, the key switch matrix from vec(S^t S) to S
+mat cli_polynomial(mat &T, mat &c, int w)
+{
+	mat S = getSecretKey(T); // get secret key
+	mat Sp = extend(S);		 // extend secret key with constant 1
+	mat t = transpose(vectorize(transpose(Sp) * Sp));
+	mat M = syscall.keySwitchMatrix(t, T);
+	return serv_polynomial(c, M, w);
+}
+
+// given ciphertext and the keyswitch matrix, computes an inner product
+vec serv_polynomial(vec c, mat &M, int w)
+{
+	vec cc = vectorize(c * transpose(c));
+	vec n = syscall.nearestInteger(cc, w);
+	return M * syscall.bit_repr_cector(n);
 }
